d60e8d90bf28909bdec03724a504e6b83fcee64e
Erivania Leal <erivanial.s1507@gmail.com>
2025-08-21 18:42:51 -0300

    Criação da estrutura do projeto
    Finalizando a classe Celula e iniciando a documentação

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # sudoku
-Um desafio para praticar POO, esse desafio faz parte do BootCamp da GFT da plataforma DIO
+Um desafio para praticar POO, esse desafio faz parte do BootCamp da GFT da plataforma DIO.
+
+## Descrição do projeto.
+> O projeto Sudoku em Java tem como objetivo implementar o jogo clássico de Sudoku no terminal. O jogador poderá iniciar um novo jogo, inserir números, remover números, visualizar o tabuleiro, verificar o status do jogo e finalizar a partida quando estiver correta.
+
+# Classe Celula.
+Representa uma posição do tabuleiro do Sudoku (9x9).
+Cada célula pode conter:
+* Um  inteiro (`valor`).
+* Uma indicação se é fixa ou não (`fixa`).
---
+++ src/sudoku/Celula.java
@@ -0,0 +1,43 @@
+package sudoku;
+
+public class Celula {
+    // criando os atributos privados para encapsulamento
+    private int valor;
+    private boolean fixa;
+
+    // inicializando o a classe com o construtor
+    public Celula(int valor, boolean fixa) {
+        this.valor = valor;
+        this.fixa = fixa;
+
+    }
+    // Getters e Setters
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if (!fixa) { // o valor so pode mudar se não for fixo
+            this.valor = valor;
+        }
+    }
+
+    public boolean isFixar() {
+        return fixa;
+    }
+
+    public void setFixar(boolean fixar) {
+        this.fixa = fixar;
+    }
+
+    // Método que limpa o valor da célula, se ela não for fixa
+    public void limpar() {
+        // Verifica se a célula NÃO é fixa (ou seja, foi preenchida pelo jogador)
+        if (!fixa) {
+            // Se não for fixa, então podemos apagar o valor (voltar para 0)
+            this.valor = 0;
+        }
+    }
+
+}
